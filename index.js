7925e7297f41ae76edc854326ee38d48dadf47e0
ImTariquL <imtariqul21@gmail.com>
2025-06-17 20:42:13 +0600

    db connect

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
-const app= express();
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.send('historical-artifacts is running...')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pw0hmwl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-app.listen(port,()=>{
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+         const artifactsCollection = client.db('artifactsdb').collection('artifacts');
+        // Test ping
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('historical-artifacts is running...')
+});
+
+app.listen(port, () => {
     console.log(`server is running from port: ${port}`);
-})
+});
