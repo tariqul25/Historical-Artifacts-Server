3e8683fb94e23dcc78de90a524a2dce347adb027
ImTariquL <imtariqul21@gmail.com>
2025-06-17 20:44:26 +0600

    artifacts post endpoint and get

--- index.js
+++ index.js
@@ -22,6 +22,34 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
          const artifactsCollection = client.db('artifactsdb').collection('artifacts');
+
+          app.get('/api/shareartifacts', async (req, res) => {
+            const result = await artifactsCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.get('/api/shareartifacts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await artifactsCollection.findOne(query);
+            res.send(result);
+        });
+
+        // get artifacts by user email
+        app.get('/api/shareartifacts/email/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { userEmail: email };
+            const result = await artifactsCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.post('/api/shareartifacts', async (req, res) => {
+            console.log('data in the server', req.body);
+            const newArtifacts = req.body;
+            const result = await artifactsCollection.insertOne(newArtifacts);
+            res.send(result);
+        });
+        
         // Test ping
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
