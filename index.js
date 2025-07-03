1b113a5f7b003d939eed3b04c537335bdacf704d
ImTariquL <imtariqul21@gmail.com>
2025-07-03 17:10:03 +0600

    update

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.use(express.json())
 var serviceAccount = require("./serviceAccount.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pw0hmwl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version  
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -32,22 +32,19 @@ async function run() {
         const likedArtifactsCollection = client.db('artifactsdb').collection('likedArtifacts');
 
         // jwt middlewares
-   const verifyJWT = async (req, res, next) => {
+        const verifyJWT = async (req, res, next) => {
             const authHeader = req?.headers?.authorization
-            console.log(authHeader)
             if (!authHeader) {
                 return res.status(401).send({ message: 'Unauthorized Access!' })
             }
 
             const token = authHeader?.split(' ')[1]
-          
+
             try {
                 const decoded = await admin.auth().verifyIdToken(token)
                 req.decoded = decoded
-                console.log(decoded)
                 next()
             } catch (err) {
-                console.log(err)
                 return res.status(401).send({ message: 'Unauthorized Access!' })
             }
         }
@@ -137,11 +134,64 @@ async function run() {
             }
         });
 
+        // Add this in your Express server code inside run()
+        app.get('/api/likedartifacts/check/:artifactId/:userEmail', async (req, res) => {
+            const { artifactId, userEmail } = req.params;
+            try {
+                const likedDoc = await likedArtifactsCollection.findOne({
+                    artifactId: new ObjectId(artifactId),
+                    userEmail,
+                });
+                res.send({ liked: !!likedDoc });
+            } catch (error) {
+                console.error(error);
+                res.status(500).send({ error: 'Server error while checking liked status' });
+            }
+        });
+
+
+        // Unlike route
+        app.patch('/api/unlike/:id', async (req, res) => {
+            const id = req.params.id;
+            const { userEmail } = req.body;
+
+            if (!userEmail) {
+                return res.status(400).send({ error: 'User email is required for unliking' });
+            }
+
+            try {
+                const likedDoc = await likedArtifactsCollection.findOne({
+                    artifactId: new ObjectId(id),
+                    userEmail: userEmail
+                });
+
+                if (!likedDoc) {
+                    return res.send({ message: 'Already unliked', modifiedCount: 0 });
+                }
+
+                await likedArtifactsCollection.deleteOne({
+                    artifactId: new ObjectId(id),
+                    userEmail: userEmail
+                });
+
+                const result = await artifactsCollection.updateOne(
+                    { _id: new ObjectId(id), liked: { $gt: 0 } },
+                    { $inc: { liked: -1 } }
+                );
+
+                res.send({ message: 'Successfully unliked', modifiedCount: result.modifiedCount });
+            } catch (err) {
+                console.error(err);
+                res.status(500).send({ error: 'Server error during unlike' });
+            }
+        });
+
+
+
 
         // Get all liked artifacts by user email
-        app.get('/api/likedartifacts/:email',verifyJWT,async (req, res) => {
+       app.get('/api/likedartifacts/user/:email', async (req, res) => {
             const email = req.params.email;
-            console.log(req.headers);
 
             try {
                 //  all liked artifact IDs by this user
@@ -174,7 +224,6 @@ async function run() {
         });
 
         app.post('/api/shareartifacts', async (req, res) => {
-            console.log('data in the server', req.body);
             const newArtifacts = req.body;
             const result = await artifactsCollection.insertOne(newArtifacts);
             res.send(result);
